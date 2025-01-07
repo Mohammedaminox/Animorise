2339ea9ba98a7da379be10cdabdbd3d765174e1b
amineBd <aminebenmade1999@gmail.com>
2025-01-07 14:42:17 +0100

    First commit

---
+++ src/main/java/com/animo/animorise/AnimoRiseApplication.java
@@ -0,0 +1,13 @@
+package com.animo.animorise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimoRiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimoRiseApplication.class, args);
+    }
+
+}
