3e185640d16d000229fde2ca866867ab517ad652
amineBd <aminebenmade1999@gmail.com>
2025-01-23 14:22:59 +0100

    jwtAuth/Add RegisterUserDto for request handling

---
+++ src/main/java/com/animo/animorise/dto/RegisterUserDto.java
@@ -0,0 +1,18 @@
+package com.animo.animorise.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+
+
+
+}
