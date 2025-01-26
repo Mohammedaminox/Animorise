927347dbc521dc02ab3ede74fb4b0b7517be2c9b
amineBd <aminebenmade1999@gmail.com>
2025-01-26 13:29:37 +0100

    remove Accessors

--- src/main/java/com/animo/animorise/response/LoginResponse.java
+++ src/main/java/com/animo/animorise/response/LoginResponse.java
@@ -2,11 +2,9 @@ package com.animo.animorise.response;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.Accessors;
 
 @Getter
 @Setter
-@Accessors(chain = true)
 public class LoginResponse {
     private String token;
     private long expiresIn;
