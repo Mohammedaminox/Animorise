8ba008a942759db9708f1f75cc4d074f50bc9c15
amineBd <aminebenmade1999@gmail.com>
2025-01-26 14:05:32 +0100

    jwtAuth/Create custom exceptions for better error handling && Global Exception Handler && Add Logging to AuthenticationService

---
+++ src/main/java/com/animo/animorise/exception/AuthenticationFailedException.java
@@ -0,0 +1,7 @@
+package com.animo.animorise.exception;
+
+public class AuthenticationFailedException extends RuntimeException {
+    public AuthenticationFailedException(String message) {
+        super(message);
+    }
+}
